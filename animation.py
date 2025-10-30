f7ed20ff17e1e690829634bbf53cf4a16ff58da5
C191239 <zsz2007314zsz@sina.com>
2025-10-29 22:58:56 -0500

    sprite & animation

---
+++ animation.py
@@ -0,0 +1,101 @@
+import copy
+from abc import ABC, abstractmethod
+from collections.abc import Iterator
+from typing import Optional
+
+from render import TILE_HEIGHT, TILE_WIDTH
+from sprite import Sprite
+
+
+class Task(Iterator[None], ABC):
+    duration: int = 0
+    progress: int = 0
+    target: Sprite
+
+    def __init__(self, target: Sprite, duration: int) -> None:
+        super().__init__()
+        self.target = copy.copy(target)
+        self.duration = duration
+
+    @abstractmethod
+    def __iter__(self) -> Iterator[None]:
+        pass
+
+    def __next__(self) -> None:
+        if self.progress >= self.duration:
+            raise StopIteration
+        else:
+            self.progress += 1
+
+    @abstractmethod
+    def reset(self) -> None:
+        pass
+
+
+class SpriteMoveTask(Task):
+    start: tuple[int, int]
+    stop: tuple[int, int]
+
+    def __init__(
+        self,
+        target: Sprite,
+        duration: int,
+        stop: tuple[int, int],
+        start: Optional[tuple[int, int]] = None,
+    ) -> None:
+        super().__init__(target, duration)
+        if start != None:
+            self.start = (start[0] * TILE_WIDTH, start[1] * TILE_HEIGHT)
+        else:
+            self.start = (
+                target.map_pos[0] * TILE_WIDTH,
+                target.map_pos[1] * TILE_HEIGHT,
+            )
+        self.stop = stop
+
+    def __iter__(self) -> Iterator[None]:
+        return self
+
+    def reset(self) -> None:
+        self.progress = 0
+        self.target.screen_pos = self.start
+        self.target.align_map_pos()
+
+    def __next__(self) -> None:
+        delta_x: int = self.stop[0] - self.start[0]
+        delta_y: int = self.stop[1] - self.start[1]
+        if self.progress < self.duration:
+            move_x: int = delta_x * (self.progress + 1) // self.duration
+            move_y: int = delta_y * (self.progress + 1) // self.duration
+            self.target.screen_pos = (self.start[0] + move_x, self.start[1] + move_y)
+            self.target.align_map_pos()
+            self.progress += 1
+            return None
+        else:
+            raise StopIteration
+
+
+class Executor:
+    pool: dict[str, Task] = {}
+
+    def add(self, name: str, task: Task) -> None:
+        self.pool[name] = task
+        task.target.animations.append(name)
+
+    def remove(self, name: str) -> Optional[Task]:
+        try:
+            task: Task = self.pool.pop(name)
+        except KeyError:
+            return None
+        task.target.animations.remove(name)
+        return task
+
+    def tick(self) -> None:
+        drained: list[str] = []
+        for name, task in self.pool.items():
+            try:
+                next(task)
+            except StopIteration:
+                drained.append(name)
+        for name in drained:
+            self.remove(name)
---
+++ sprite.py
@@ -0,0 +1,103 @@
+import copy
+from abc import ABC, abstractmethod
+from typing import Callable, Optional
+
+import pygame as pg
+
+import animation as ani
+import render
+from render import TILE_HEIGHT, TILE_WIDTH
+
+
+class Sprite(ABC):
+    map_pos: tuple[int, int]
+    screen_pos: tuple[int, int]
+    animations: list[str] = []
+
+    @abstractmethod
+    def draw(self) -> None:
+        """Draw the sprite to the screen."""
+        pass
+
+    def align_map_pos(self) -> None:
+        """
+        Align `map_pos` with the current `screen_pos`.
+        """
+        self.map_pos = (
+            self.screen_pos[0] // TILE_WIDTH,
+            self.screen_pos[1] // TILE_HEIGHT,
+        )
+
+    def align_screen_pos(self) -> None:
+        """
+        Align `screen_pos` with the current `map_pos`.
+        """
+        self.screen_pos = (self.map_pos[0] * TILE_WIDTH, self.map_pos[1] * TILE_WIDTH)
+
+
+class TextureSprite(Sprite):
+    surface: pg.Surface
+    texture: tuple[int, int]
+
+    def __init__(
+        self, surface: pg.Surface, texture: tuple[int, int], position: tuple[int, int]
+    ) -> None:
+        self.map_pos = position
+        self.screen_pos = (position[0] * TILE_WIDTH, position[1] * TILE_HEIGHT)
+        self.surface = copy.copy(surface)
+        self.texture = texture
+
+    def draw(self) -> None:
+        render.draw_tile_custom(self.surface, self.texture, self.screen_pos)
+
+
+class CustomSprite(Sprite):
+    user_data: dict[str, object]
+    draw_func: Callable[[tuple[int, int], dict[str, object]], None]
+
+    def __init__(
+        self,
+        position: tuple[int, int],
+        user_data: dict[str, object],
+        draw_func: Callable[[tuple[int, int], dict[str, object]], None],
+    ) -> None:
+        self.map_pos = position
+        self.user_data = user_data
+        self.draw_func = draw_func
+
+    def draw(self) -> None:
+        self.draw_func(self.map_pos, self.user_data)
+
+
+class Spawner:
+    sprite_pool: dict[str, Sprite] = {}
+    animation_exec: ani.Executor = ani.Executor()
+
+    def add_sprite(self, name: str, sprite: Sprite) -> None:
+        self.sprite_pool[name] = sprite
+
+    def remove_sprite(self, name: str) -> tuple[Sprite, list[ani.Task]]:
+        sprite: Sprite = self.sprite_pool.pop(name)
+        tasks: list[ani.Task] = [
+            task
+            for task in [
+                self.animation_exec.remove(task) for task in sprite.animations.copy()
+            ]
+            if task != None
+        ]
+        return (sprite, tasks)
+
+    def add_animation(self, name: str, task: ani.Task) -> Optional[ani.Task]:
+        if task.target in self.sprite_pool:
+            self.animation_exec.add(name, task)
+            return None
+        else:
+            return task
+
+    def remove_animation(self, name: str) -> Optional[ani.Task]:
+        return self.animation_exec.remove(name)
+
+    def tick(self) -> None:
+        self.animation_exec.tick()
+        for sprite in self.sprite_pool.values():
+            sprite.draw()
