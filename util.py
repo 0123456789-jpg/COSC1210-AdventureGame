28814aaf293010dc333acd4755238662fa43acdd
C191239 <zsz2007314zsz@sina.com>
2025-10-30 20:23:24 -0500

    fix circular import

--- util.py
+++ util.py
@@ -1,9 +1,10 @@
-from render import TILE_HEIGHT, TILE_WIDTH
-
-
 def screen_to_map(screen: tuple[int, int]) -> tuple[int, int]:
+    from render import TILE_HEIGHT, TILE_WIDTH
+
     return (screen[0] // TILE_WIDTH, screen[1] // TILE_HEIGHT)
 
 
 def map_to_screen(map: tuple[int, int]) -> tuple[int, int]:
+    from render import TILE_HEIGHT, TILE_WIDTH
+
     return (map[0] * TILE_WIDTH, map[1] * TILE_HEIGHT)
